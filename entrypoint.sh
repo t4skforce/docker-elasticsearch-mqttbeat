61dcd11ec10a120b6608e86077a138a9c9e781c5
t4skforce <t4skforce1337@gmail.com>
2018-08-18 17:57:43 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,8 @@ set -e
 if [ ! -f /etc/mqttbeat/mqttbeat.yml ]; then
   cp /etc/default/mqttbeat.yml /etc/mqttbeat/mqttbeat.yml
 fi
+if [ ! -f /etc/mqttbeat/fields.yml ]; then
+  cp /etc/default/fields.yml /etc/mqttbeat/fields.yml
+fi
 
 exec "$@"
